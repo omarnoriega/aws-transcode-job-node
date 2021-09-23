353cd0780b08ad58f30f7899ab90440670c221e1
Omar Noriega <omaranoriega@gmail.com>
2021-09-23 10:02:47 -0500

    Update

--- index.ts
+++ index.ts
@@ -46,3 +46,15 @@ const main = async () => {
 };
 
 main();
+
+
+/*
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
+*/
